deb86fd87b71c54a58a8fc64645d5cceca4b67da
Leni-Sintiasari <leni_sintiasari_33rpl@student.smktelkom-mlg.sch.id>
2024-08-02 13:58:04 +0700

    Add files via upload

---
+++ Abc.java
@@ -0,0 +1,6 @@
+public class Abc {
+    public static void main(String[] args) {
+        System.out.println("Nama saya Leni dari kelas xrpl 7 yang berasal dari kota Malang");
+        String a = "1";
+    }
+}
