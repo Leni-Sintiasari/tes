aa19acabf10e8aeb26a5ea7005cf13c896cea394
Leni-Sintiasari <leni_sintiasari_33rpl@student.smktelkom-mlg.sch.id>
2024-08-05 11:37:38 +0700

    Add files via upload

---
+++ Data.java
@@ -0,0 +1,15 @@
+public class Data {
+    public static void main(String[] args) {
+       //tipe_data nama_var = value/isi var;
+        String nama = "Leni";
+        int usia = 16;
+        String alamat = "Jl. Karimun Jawa";
+        double kelas = 10.5;
+
+        System.out.println("Nama saya : " + nama);
+        System.out.println("Usia : " + usia + "tahun");
+        System.out.println("Alamat : " + alamat);
+        System.out.println("Kelas : " + kelas);
+    }
+
+}
