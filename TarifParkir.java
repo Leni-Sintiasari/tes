b7f8ded7d694c441f7abfb8e4078116b0221d4aa
Leni-Sintiasari <leni_sintiasari_33rpl@student.smktelkom-mlg.sch.id>
2024-09-06 10:41:49 +0700

    Add files via upload

---
+++ TarifParkir.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class TarifParkir {
+    public static void main(String[] args) {
+        String platNomor;
+        int jumlahJam;
+        double harga;
+
+        Scanner s = new Scanner(System.in);
+        System.out.print("Masukkan Nomor Plat: ");
+        platNomor = s.nextLine();
+        System.out.print("Jumlah Jam: ");
+        jumlahJam = s.nextInt();
+
+        if(jumlahJam >0 && jumlahJam <=1){
+            harga=3000;
+        }else if(jumlahJam >1 && jumlahJam<=4){
+            harga=7000;
+        }else if(jumlahJam > 4){
+            harga = 10000;
+        }else{
+            harga = 0;
+            System.out.println("System Eror");
+        }
+
+        System.out.println(platNomor);
+        System.out.println("Tarif Parkir "+harga);
+
+        
+    }
+    
+}
